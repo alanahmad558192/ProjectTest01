4643419ba1eec35cfb97c9d3265c398ce1118f43
Alan Ahmad <aahmad@sevenseasworldwide.com>
2024-03-26 17:41:14 +0800

    Testing a Service

---
+++ src/app/message.service.spec.ts
@@ -0,0 +1,27 @@
+import { MessageService } from "./message.service"
+
+describe('MessageService', () => {
+    let service: MessageService
+
+    beforeEach(() => {
+        service = new MessageService();
+    });
+
+    it('should have no message to start', () => {
+        expect(service.messages.length).toBe(0);
+    })
+
+    it('should add a message when add is called', () => {
+        service.add('message');
+
+        expect(service.messages.length).toBe(1);
+    })
+
+    it ('should remove all messages when clear is called', () => {
+        service.add('message');
+
+        service.clear();
+
+        expect(service.messages.length).toBe(0);
+    })
+})
