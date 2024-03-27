247cef9376f44cf9b05b7496f077668dfb1effa0
Alan Ahmad <aahmad@sevenseasworldwide.com>
2024-03-27 14:17:52 +0800

    TestBed test on Hero Component

---
+++ src/app/hero/hero.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HeroComponent } from './hero.component';
+import { Hero } from '../hero';
+
+//Shallow Test for HeroComponent
+fdescribe('HeroComponent', () => {
+
+    let fixture: ComponentFixture<HeroComponent>;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations: [HeroComponent]
+        });
+        fixture = TestBed.createComponent(HeroComponent);
+    });
+
+    it('should have the correct hero', () => {
+        fixture.componentInstance.hero = {id: 1, name: 'SuperDude', strength: 3};
+        // fixture.detectChanges();
+
+        expect(fixture.componentInstance.hero.name).toEqual('SuperDude');
+    });
+});
